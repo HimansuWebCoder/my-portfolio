6751452b592d0369db885f09c7b59d43bc816a03
himansu naik <naikhimansu546@gmail.com>
2024-10-04 17:46:59 +0530

    update font family in profile name

--- frontend/src/Components/About-card/AboutCard.js
+++ frontend/src/Components/About-card/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
 				<img src="/logo.jpg" alt="img" />
 			</div>
 			<hr className="bar" />
-			<h1>Himansu Naik</h1>
+			<h1 id="profile-name">Himansu Naik</h1>
 			<div className="about-description-container">
 				<p>
 					Hii, I am Himansu, Full-stack Javascript Developer, Welcome

2ba369e1a22361f560fdf60b23db03271e85757c
Himansu Naik <123152485+HimansuWebCoder@users.noreply.github.com>
2024-10-04 08:04:30 +0530

    Update nested route by using * after / in layout parent routes in App js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout />}>
+          <Route path="/*" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="skills" element={<Skills />} />

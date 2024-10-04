f88066253681a133de9bd36f03254e092d89d54e
himansu naik <naikhimansu546@gmail.com>
2024-10-04 13:17:23 +0530

    updated contact and skillscard

--- frontend/src/Routes/Contact/Contact.js
+++ frontend/src/Routes/Contact/Contact.js
@@ -4,7 +4,38 @@ import "./Contact.css";
 function Contact() {
 	return (
 		<div className="contact-container">
-			<div>Email: naikhimansu546@gmail.com</div>
+			<div className="contact-sub-container">
+				<div>
+					<h2>Contact Me</h2>
+					<a
+						href="tel:+91-9861194145"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						+919861194145
+					</a>
+				</div>
+				<div>
+					<h2>Email:</h2>
+					<a
+						href="intent://send?to=naikhimansu546@gmail.com&subject=Inquiry&body=Hello,%20I%20would%20like%20to%20inquire%20about...#Intent;scheme=gmail;package=com.google.android.gm;end"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						naikhimansu546@gmail.com
+					</a>
+				</div>
+				<div>
+					<h2>WhatsApp:</h2>
+					<a
+						href="https://wa.me/9861194145"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Message Me on WhatsApp
+					</a>
+				</div>
+			</div>
 		</div>
 	);
 }

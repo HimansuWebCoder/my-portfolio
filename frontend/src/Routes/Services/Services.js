49a42c44b86b12ea2464fb0be9b8bbdcc304900f
himansu naik <naikhimansu546@gmail.com>
2024-10-04 15:51:56 +0530

    add services content

--- frontend/src/Routes/Services/Services.js
+++ frontend/src/Routes/Services/Services.js
@@ -1,9 +1,27 @@
-import RocketCard from "../../Components/Rocket-card/RocketCard";
+// import RocketCard from "../../Components/Rocket-card/RocketCard";
+import "./Services.css";
 
 function Services() {
 	return (
-		<div>
-			<h1>Services</h1>
+		<div className="Services-container">
+			<div className="Service-bucket">
+				<h1> Frontend Web Application Development</h1>
+				<hr />
+				<h1> Backend Web Application Development</h1>
+				<hr />
+				<h1> Full-Stack Web Application Development</h1>
+				<hr />
+				<h1> Website Development</h1>
+				<hr />
+
+				<h3>Category</h3>
+				<select>
+					<option>E-Commerce</option>
+					<option>Social Media</option>
+					<option>Blog Application</option>
+					<option>Chat Application</option>
+				</select>
+			</div>
 		</div>
 	);
 }

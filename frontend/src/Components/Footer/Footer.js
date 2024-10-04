9e67ce45e58675fb29796dd9806749d7ff5dc47d
himansu naik <naikhimansu546@gmail.com>
2024-10-04 18:04:18 +0530

    add footer section

---
+++ frontend/src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer() {
+	return (
+		<div className="footer-container">
+			© 2024 Himansu Web Portfolio. All rights reserved.
+		</div>
+	);
+}
+
+export default Footer;
--- frontend/src/Container/Layout/Layout.js
+++ frontend/src/Container/Layout/Layout.js
@@ -1,12 +1,14 @@
 import { Outlet, Link } from "react-router-dom";
 
 import Navigation from "../../Components/Navigation/Navigation";
+import Footer from "../../Components/Footer/Footer";
 
 function Layout() {
 	return (
 		<div>
 			<Navigation />
 			<Outlet />
+			<Footer />
 		</div>
 	);
 }

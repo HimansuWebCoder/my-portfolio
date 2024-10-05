d598e6c6d7bbf7f5bd97defbfe62e4fddd91a15e
himansu naik <naikhimansu546@gmail.com>
2024-10-05 21:55:15 +0530

    add gif and update frontend cards style

--- frontend/src/Container/Layout/Layout.js
+++ frontend/src/Container/Layout/Layout.js
@@ -2,10 +2,11 @@ import { Outlet, Link } from "react-router-dom";
 
 import Navigation from "../../Components/Navigation/Navigation";
 import Footer from "../../Components/Footer/Footer";
+import "./Layout.css";
 
 function Layout() {
 	return (
-		<div>
+		<div className="layout-container">
 			<Navigation />
 			<Outlet />
 			<Footer />
